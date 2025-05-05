9db0eb628cf5a87e8112c74dddd209a43bc1c593
Alexfp28 <alexlopezdelafuente@gmail.com>
2025-05-06 00:08:05 +0200

    Subida a produccion con rutas para GitHub Pages supuestamente arreglada.

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,5 @@
-import {createRouter, createWebHistory} from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import HomeView from "@/views/HomeView.vue";
 import AppointmentView from "@/views/AppointmentView.vue";
 import CatalogView from "@/views/CatalogView.vue";
 import ContactView from "@/views/ContactView.vue";
@@ -8,18 +8,13 @@ import TermsAndConditionsView from "@/views/TermsAndConditionsView.vue";
 import LoginView from "@/views/LoginView.vue";
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory('/newman-front/'), // Asegúrate de que el nombre coincida con el `base` configurado en vite.config.js
   routes: [
     {
       path: '/home',
       name: 'home',
       component: HomeView,
     },
-    // {
-    //   path: '/schedule',
-    //   name: 'schedule',
-    //   component: ScheduleView,
-    // },
     {
       path: '/appointment',
       name: 'appointment',
@@ -40,11 +35,6 @@ const router = createRouter({
       name: 'profileSettings',
       component: UserProfileSettingsView,
     },
-    // {
-    //   path: '/userHistory',
-    //   name: 'userHistory',
-    //   component: UserHistoryView,
-    // },
     {
       path: '/termsandconditions',
       name: 'termsandconditions',
@@ -66,14 +56,12 @@ const router = createRouter({
     }
   ],
   scrollBehavior(to, from, savedPosition) {
-
     if (savedPosition) {
       return savedPosition;
     } else {
-
       return { top: 0 };
     }
   }
-})
+});
 
-export default router
+export default router;
